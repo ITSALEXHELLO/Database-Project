9379864a1e6616b73206bc4ee9dd03ecc079d8ed
Pranav Shridhar <pranavshridhar0@gmail.com>
2024-11-19 22:01:20 -0600

    Stabilized Flask server, provided environment file for Conda

--- be/app.py
+++ be/app.py
@@ -1,8 +1,7 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-api = Api(app)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def hello():
+    return "Hello, World!"
