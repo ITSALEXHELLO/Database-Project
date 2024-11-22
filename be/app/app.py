600c3541c8d48c5c2995b2e492dd22b8eaa1c822
Dhanush <dhanushbommavaram@gmail.com>
2024-11-21 19:51:45 -0600

    createCustomer endpoint

--- be/app/app.py
+++ be/app/app.py
@@ -19,7 +19,7 @@ connection = mysql.connector.connect(
     )
 
 
-@app.route("/menu/")
+@app.route("/menu", methods=["GET"])
 def getMenu():
     cursor = connection.cursor(dictionary=True)
     cursor.execute('SELECT * FROM MenuItem')  # Query the menu table
@@ -27,6 +27,34 @@ def getMenu():
     cursor.close()
     
     return jsonify(menu_items)
+
+@app.route("/createCustomer", methods=["POST"])
+def createCustomer():
+    # Get JSON data from the request body
+    data = request.get_json()
+
+    # Extract the fields from the JSON data
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    phone_number = data.get("phone_number")
+    email = data.get("email")
+
+    cursor = connection.cursor(dictionary=True)
+
+    try:
+        # Insert the new user into the users table
+        cursor.execute('INSERT INTO Customer(first_name,last_name,phone_number,email) VALUES (%s, %s, %s, %s)', (first_name,last_name,phone_number,email))
+        connection.commit()
+    except Exception as err:
+        connection.rollback()  # Rollback any changes if an error occurs
+        return jsonify({"error": f"Database error: {err}"}), 500
+    finally:
+        cursor.close()
+
+    # Return a success message with the newly created user data
+    return jsonify({
+        "message": "User created successfully"
+    }), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
